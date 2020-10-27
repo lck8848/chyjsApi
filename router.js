2919f9e39720e8c7eb95e8bd2d8c67b9c266a5b5
youloby <1696279132@qq.com>
2020-10-27 23:37:30 +0800

    修改购物车接口

--- router.js
+++ router.js
@@ -75,4 +75,10 @@ route.post('/api/updateAddr', controller.updateAddr);
 
 route.get('/api/deleteAddr', controller.deleteAddr);
 
+route.get('/api/getCartList', controller.getCartList);
+
+route.get('/api/getSpec', controller.getSpec);
+
+route.post('/api/updateCart', controller.updateCart);
+
 module.exports = route;
